7adf01b84ab71a7991408416c6f98a1123d39d11
Shokhboz Khalilov <54911697+Shoxboss@users.noreply.github.com>
2021-07-12 14:05:30 +0300

    Update Scene.java

--- src/main/java/com/practice/Gui/Scene.java
+++ src/main/java/com/practice/Gui/Scene.java
@@ -15,15 +15,55 @@ public class Scene extends JPanel {
 	private ArrayList<Rib> ribs;
 	private HashMap<String, Vertex> verticesDict;
 
+	private boolean isVisabel = true;
+
+	public void setVisabel(boolean flag) {
+		isVisabel = flag;
+	}
+
 	public ArrayList<Rib> getRibList(){
 		return ribs;
 	}
 
 	public void addRib( Rib rib ) {
 		ribs.add(rib);
-		add(rib.getComponent());
+		Board Jc = rib.getComponent();
+		add(Jc);
+		rib.getSourceVertex().addMouseListener( new MouseAdapter() {
+			@Override
+			public void mouseClicked( MouseEvent e ) {
+				super.mouseClicked( e );
+				if( isVisabel ) {
+					if(Main.currentOption == Main.Option.DELETE) {
+								remove( Jc );
+						}
+					}
+				}
+			} );
+		Jc.addMouseListener(new MouseAdapter(){
+						
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if( isVisabel ) { 						
+					if(Main.currentOption == Main.Option.DELETE) { 
+						
+						for(int i = 0; i < ribs.size(); i++) {
+							
+							if(  Jc.getParentComp().getSourceVertex() ==  ribs.get(i).getSourceVertex()  
+								&& Jc.getParentComp().getTargetVertex() == ribs.get(i).getTargetVertex()) {
+									ribs.remove(i);
+							}
+						}			
+						remove(Jc);
+						repaint();										
+					}
+				}
+			}
+		});
+
 		repaint();
 	}
+	
 
 	Scene() {
 		setOpaque( false );
@@ -36,123 +76,129 @@ public class Scene extends JPanel {
 		addMouseMotionListener( new MouseAdapter() {
 			@Override
 			public void mouseMoved( MouseEvent e ) {
-				super.mouseMoved( e );
-				to = e.getPoint();
-				validate();
-				repaint();
+				if( isVisabel ) { 
+
+					super.mouseMoved( e );
+					to = e.getPoint();
+					validate();
+					repaint();
+				}
 			}
 		} );
 	}
 
 
     public void addVertex( Vertex vertex ){
-
+		
 		vertex.addMouseListener( new MouseAdapter() {
 			@Override
 			public void mouseClicked( MouseEvent e ) {
 				super.mouseClicked( e );
-				if(Main.currentOption == Main.Option.CONNECT) {
-
-					if( rib == null ) {
-						to = from = vertex.getCenterPoint();
-						rib = new Rib();
-						rib.setSourceVertex( vertex );
-						vertex.setColour( Color.cyan );
-						validate();
-						repaint();
-
-					} else if( rib.isFull() == false && rib.getSourceVertex() != vertex ) {
-
-						Integer weight = 0;
-						String answer;
-						String msg = "напишите вес ребра";
-				
-						int optionPane = JOptionPane.QUESTION_MESSAGE;
-				
-						for( int isNumber = 0; isNumber < 1;  ) {
-				
-							answer = JOptionPane.showInputDialog(null, msg,
-									"Вес ребра", optionPane);
-							if(answer == null) {
-								
-								rib.getSourceVertex().setColour( Color.lightGray );
-								rib = null;
-								validate();
-								repaint();
-								return;
-							}	
-							try {
-								weight = Integer.parseInt(answer);
-								if( weight != 0 ) {
-									isNumber = 1;
-								}else {
-									msg = "вес ребра должен быть целым числом больше нуля";
-									optionPane = JOptionPane.WARNING_MESSAGE;
-								}
-							}
-							catch (NumberFormatException err)
-							{
-								msg = "вес ребра должно быть целое натуральное число";
-								optionPane = JOptionPane.ERROR_MESSAGE;
-								continue;
-							}
-						}
-											
-						to = vertex.getCenterPoint();
-						rib.setTargetVertex( vertex );
-						rib.getSourceVertex().setColour( Color.lightGray );
-						rib.setWeight( weight );
-
-						Board Jc = rib.getComponent();
-						add( Jc );
-
-						rib.getSourceVertex().addMouseListener( new MouseAdapter() {
-							@Override
-							public void mouseClicked( MouseEvent e ) {
-								super.mouseClicked( e );
-								if(Main.currentOption == Main.Option.DELETE) {
-									remove( Jc );
-								}
-							}
-						} );
-
-						vertex.addMouseListener( new MouseAdapter() {
-							@Override
-							public void mouseClicked( MouseEvent e ) {
-								super.mouseClicked( e );
-								if(Main.currentOption == Main.Option.DELETE) {
-									remove( Jc );
-								}
-							}
-						} );
-
-						Jc.addMouseListener(new MouseAdapter(){
+				if( isVisabel ) {
 						
-							@Override
-							public void mouseClicked(MouseEvent e) {
+					if(Main.currentOption == Main.Option.CONNECT) {
 
-								if(Main.currentOption == Main.Option.DELETE) { 
+						if( rib == null ) {
+							to = from = vertex.getCenterPoint();
+							rib = new Rib();
+							rib.setSourceVertex( vertex );
+							vertex.setColour( Color.cyan );
+							validate();
+							repaint();
+
+						} else if( rib.isFull() == false && rib.getSourceVertex() != vertex ) {
+
+							Integer weight = 0;
+							String answer;
+							String msg = "напишите вес ребра";
+					
+							int optionPane = JOptionPane.QUESTION_MESSAGE;
+					
+							for( int isNumber = 0; isNumber < 1;  ) {
+					
+								answer = JOptionPane.showInputDialog(null, msg,
+										"Вес ребра", optionPane);
+								if(answer == null) {
 									
-									for(int i = 0; i < ribs.size(); i++) {
-										
-										if(  Jc.getParentComp().getSourceVertex() == ribs.get(i).getSourceVertex()
-											&& Jc.getParentComp().getTargetVertex() == ribs.get(i).getTargetVertex()) {
-												ribs.remove(i);
-										}
-									}			
-									remove(Jc);
-									repaint();										
+									rib.getSourceVertex().setColour( Color.lightGray );
+									rib = null;
+									validate();
+									repaint();
+									return;
+								}	
+								try {
+									weight = Integer.parseInt(answer);
+									if( weight != 0 ) {
+										isNumber = 1;
+									}else {
+										msg = "вес ребра должен быть целым числом больше нуля";
+										optionPane = JOptionPane.WARNING_MESSAGE;
+									}
+								}
+								catch (NumberFormatException err)
+								{
+									msg = "вес ребра должно быть целое натуральное число";
+									optionPane = JOptionPane.ERROR_MESSAGE;
+									continue;
 								}
 							}
-						});
+												
+							to = vertex.getCenterPoint();
+							rib.setTargetVertex( vertex );
+							rib.getSourceVertex().setColour( Color.lightGray );
+							rib.setWeight( weight );
 
-						ribs.add( rib );
-						rib = null;
-						validate();
-						repaint();
+							Board Jc = rib.getComponent();
+							add( Jc );
+
+							if (isVisabel) {
+								rib.getSourceVertex().addMouseListener( new MouseAdapter() {
+									@Override
+									public void mouseClicked( MouseEvent e ) {
+										super.mouseClicked( e );
+										if(Main.currentOption == Main.Option.DELETE) {
+											remove( Jc );
+										}
+									}
+								} );
+
+								vertex.addMouseListener( new MouseAdapter() {
+									@Override
+									public void mouseClicked( MouseEvent e ) {
+										super.mouseClicked( e );
+										if(Main.currentOption == Main.Option.DELETE) {
+											remove( Jc );
+										}
+									}
+								} );
+
+								Jc.addMouseListener(new MouseAdapter(){
+								
+									@Override
+									public void mouseClicked(MouseEvent e) {
+
+										if(Main.currentOption == Main.Option.DELETE) { 
+											
+											for(int i = 0; i < ribs.size(); i++) {
+												
+												if(  Jc.getParentComp().getSourceVertex() ==  ribs.get(i).getSourceVertex()  
+													&& Jc.getParentComp().getTargetVertex() == ribs.get(i).getTargetVertex()) {
+														ribs.remove(i);
+												}
+											}			
+											remove(Jc);
+											repaint();										
+										}
+									}
+								});
+							}
+							ribs.add( rib );
+							rib = null;
+							validate();
+							repaint();
+						}
 					}
-				}
-				else if( Main.currentOption == Main.Option.DELETE ) {
+					else if( Main.currentOption == Main.Option.DELETE ) {
 					
 					
 					for(int i = ribs.size()-1; i >= 0; i--) {
@@ -164,12 +210,14 @@ public class Scene extends JPanel {
 					revalidate();
 					repaint();
 				}
+				}
 			}
 			@Override
 			public void mousePressed( MouseEvent e ) {
 				super.mousePressed( e );
 			}
 		} );
+		
 		verticesDict.put(vertex.getId(), vertex);
 		add(vertex);
         validate();
@@ -215,13 +263,15 @@ public class Scene extends JPanel {
 		for (Edge edge : edges){
 			Rib rib = new Rib();
 			rib.setWeight(edge.getWeight());
+
+			
+
 			rib.setSourceVertex(verticesDict.get(edge.getStartName()));
 			System.out.println(verticesDict.get(edge.getStartName()).getId());
 			rib.setTargetVertex(verticesDict.get(edge.getEndName()));
 			System.out.println(verticesDict.get(edge.getEndName()).getId());
-			//rib.setComponent(new Board(String.valueOf(edge.getWeight()), 0, 0));
-			//rib.setWeight(edge.getWeight());
-			rib.setColor( edge.getStart().getComponent());
+		
+			rib.setColor( Math.min( edge.getStart().getComponent(), edge.getEnd().getComponent() ));
 			
 			this.addRib(rib);
 		}
